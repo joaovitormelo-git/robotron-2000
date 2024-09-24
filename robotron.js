c88ed48c991438014d06470126d6ddebc17eaa87
joaovitormelo-git <jvbsm1@aluno.ifal.edu.br>
2024-09-24 14:43:14 -0300

    Add files via upload

---
+++ robotron.js
@@ -0,0 +1,43 @@
+// const cores = {
+//     cores: ['img/robotron-vermelho.png', 'img/robotron-amarelo.png', 'img/robotron-branco.png',
+//         'img/robotron-preto.png', 'img/robotron-rosa.png', 'img/robotron-azul.png'],
+
+//     percorrer: function () {
+//         for (let i = 0; i < cores; i++) {
+//             return cores.length
+//         }
+//     }
+
+// }
+
+let cores = ['.img/robotron-vermelho.png', '.img/robotron-amarelo.png', '.img/robotron-branco.png',
+'.img/robotron-preto.png', '.img/robotron-rosa.png', '.img/robotron-azul.png']
+let corAtual = 0
+
+let roboImagem = document.querySelector('.robo')
+
+function percorrer(cores) {
+    for (let i = 0; i < cores; i++)
+    return cores.length
+}
+
+const esquerda = document.querySelector('.esquerda')
+let robo = document.querySelector('.robo')
+esquerda.document.addEventListener('click', function () {
+    esquerda.style.percorrer()
+    return robotron
+})
+const direita = document.querySelector('.direita')
+let robo = document.querySelector('.robo')
+direita.document.addEventListener('click', function () {
+    direita.style.percorrer()
+    return robotron
+})
+
+// let cores = ['img/robotron-vermelho.png', 'img/robotron-amarelo.png', 'img/robotron-branco.png',
+// 'img/robotron-preto.png', 'img/robotron-rosa.png', 'img/robotron-azul.png']
+// function percorrer(cores) {
+//     for (let i = 0; i < cores; i++)
+//     return cores.length
+// }
+
